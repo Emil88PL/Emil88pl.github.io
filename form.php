9a3cd0161f5f581d144969e4ef756661ab33b1cd
Emil <emil88pl@gmail.com>
2017-11-27 19:01:26 +0000

    Update form.php

--- form.php
+++ form.php
@@ -24,10 +24,10 @@
 
   $headers .= "Reply-To: $visitor_email \r\n";
 
-  @mail($to,$email_subject,$email_body,$headers);
+  mail($to,$email_subject,$email_body,$headers);
 
 
-
+echo  $visitor_email
 
 
 
