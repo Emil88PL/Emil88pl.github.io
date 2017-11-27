e5055c84c9c48790c2943dd5e6c1662d92bee311
Emil <emil88pl@gmail.com>
2017-11-27 17:05:26 +0000

    Update form.php

--- form.php
+++ form.php
@@ -1,5 +1,37 @@
 <?php
 
+  $name = $_GET['name'];
+
+  $visitor_email = $_GET['email'];
+
+  $message = $_GET['message'];
+
+
+
+  $email_from = 'Emil88pl@gmail.com';
+
+  $email_subject = "New Message";
+
+  $email_body = "You have received a new message from the user $name.\n".
+                            "Here is the message:\n $message".
+
+
+
+
+  $to = "Emil88pl@gmail.com";
+
+  $headers = "From: $email_from \r\n";
+
+  $headers .= "Reply-To: $visitor_email \r\n";
+
+  mail($to,$email_subject,$email_body,$headers);
+
+
+
+
+
+
+
 function IsInjected($str)
 {
     $injections = array('(\n+)',
@@ -29,32 +61,6 @@ if(IsInjected($visitor_email))
     echo "Bad email value!";
     exit;
 }
-
-  $name = $_GET['name'];
-
-  $visitor_email = $_GET['email'];
-
-  $message = $_GET['message'];
-
-
-
-  $email_from = 'Emil88pl@gmail.com';
-
-  $email_subject = "New Message";
-
-  $email_body = "You have received a new message from the user $name.\n".
-                            "Here is the message:\n $message".
-
-
-
-
-  $to = "Emil88pl@gmail.com";
-
-  $headers = "From: $email_from \r\n";
-
-  $headers .= "Reply-To: $visitor_email \r\n";
-
-  mail($to,$email_subject,$email_body,$headers);
 
 
 
