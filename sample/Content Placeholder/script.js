deccc85ae9e3e7b24e3e9b7099512cff21d835cb
Emil <emil88pl@gmail.com>
2021-11-25 15:01:22 +0000

    Add files via upload

---
+++ sample/Content Placeholder/script.js
@@ -0,0 +1,23 @@
+const header = document.getElementById('header')
+const title = document.getElementById('title')
+const excerpt = document.getElementById('excerpt')
+const profile_img = document.getElementById('profile_img')
+const name = document.getElementById('name')
+const date = document.getElementById('date')
+
+const animated_bgs = document.querySelectorAll('.animated-bg')
+const animated_bg_text = document.querySelectorAll('.animated-bg-text')
+
+setTimeout(getData, 500)
+
+function getData(){
+    header.innerHTML = '<img src="img/lap.png" alt="">'
+    title.innerHTML = 'Lorem ipsum dolor sit amet.'
+    excerpt.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipisicing.'
+    profile_img.innerHTML = '<img src="https://randomuser.me/api/portraits/men/15.jpg" alt="">'
+    name.innerHTML = 'Name XXX'
+    date.innerHTML = 'Oct 10, 2021'
+
+    animated_bgs.forEach(bg => bg.classList.remove('animated-bg'))
+    animated_bg_text.forEach(bg => bg.classList.remove('animated-bg'))
+}
